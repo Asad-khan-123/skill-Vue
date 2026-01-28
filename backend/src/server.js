3b15b3dc0d61474bdf0a0e6af0cbdd015adb9a48
asad <asadkhan997759@gmail.com>
2026-01-28 14:04:33 +0530

    change port

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ if(ENV.NODE_ENV === 'production'){
 app.get("/{*any}", (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
 })
-const PORT = 8000;
+const PORT = ENV.PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port  http://localhost:${PORT}/`);
 })  
